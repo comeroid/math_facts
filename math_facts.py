38e9fa1c23a61762c8a49865e3cd1f16d7df83e5
caleb <comer.children@gmail.com>
2015-09-25 13:41:53 -0600

    Add math_facts.py to application

---
+++ math_facts.py
@@ -0,0 +1,66 @@
+import random
+#if name=="__main__":
+
+def main():
+  print "welcome to math facts!"
+  print "do you want to do multiplication or division?"
+  print "m)ultiplication or d)ivision"
+  choice = raw_input()   
+  num_questions = 2
+  counter_answers = 0
+  for i in range(num_questions): 
+    multiplicand = random.randint(1,12)
+    multiplier = random.randint(1,12)
+    product = multiplicand * multiplier
+    print "{} * {} = ?".format(multiplicand,multiplier)
+    answer = input()
+    if answer== product:
+      print "correct!" 
+      counter_answers = counter_answers + 1
+    else:
+        print "incorrect!"
+  print "congratulations you got %d/%d  correct" % (counter_answers,num_questions)
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
